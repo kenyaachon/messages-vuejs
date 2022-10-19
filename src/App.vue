1037d8aba90b63423c9080f0caf2a012a37a17c1
mosesmbugua <swahili1963@gmail.com>
2022-10-18 21:20:05 -0700

    Simple Backend

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Messages from "./components/Messages.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <Messages />
 </template>
 
 <style scoped>
---
+++ src/components/Messages.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4>Messages</h4>
+    <!-- <ul v-for="(message, index) in messages" :key="index">
+      <li>{{ message }}</li>
+    </ul> -->
+    <Listbox v-model="message" :options="messages" optionLabe="name" />
+  </div>
+</template>
+
+<script lang="ts">
+import Listbox from "primevue/listbox";
+export default {
+  data() {
+    return {
+      messages: ["what is up my guy", "hi", "We are"],
+      message: "",
+    };
+  },
+  components: {
+    Listbox,
+  },
+};
+</script>
+
+<style></style>
